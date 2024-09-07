c5ce77d9e3c06a0b5c5c8667c9f661debb661a0f
Txtdatboy <herokukeliy@outlook.com>
2024-09-07 18:42:54 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) ACE 
+
+import os
+
+class Config(object):
+    # get a token from @BotFather
+    pass
+    """
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6997863912:AAFNUgXwZaxY8KlgvvGkeYMh6sDC0rNVDM0")
+    API_ID = int(os.environ["API_ID", 20088962]
+    API_HASH = os.environ["API_HASH", "257f47d347157555890a64b12bc0134f"]
+    AUTH_USERS = "6169016546 """
+
